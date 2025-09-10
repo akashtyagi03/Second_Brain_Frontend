f81e1de6224a80d491bb2e0e7c7fa641b3cf3865
Aakash tyagi <akashtyagi6065@gmail.com>
2025-09-11 02:18:30 +0530

    add button component

--- src/component/ui/Button.tsx
+++ src/component/ui/Button.tsx
@@ -1,12 +1,27 @@
+import type { ReactElement } from "react";
+
 interface ButtonProps {
     variant: "primary" | "secondary";
     size: "sm" | "md" | "lg";
     text: string;
-    starticon?: any;
-    endicon?: any;
+    starticon?: ReactElement;
+    endicon?: ReactElement;
     onClick?: () => void;
 }
 
+const variantstyle = {
+    "primary":"bg-blue-500 text-white",
+    "secondary":"bg-gray-500 text-white"
+}
+const sizestyle = {
+    "sm":"px-2 py-1 text-sm",
+    "md":"px-4 py-2 text-md",
+    "lg":"px-6 py-3 text-lg"
+}
+const defaultstyle = "font-semibold rounded flex gap-2";
+
 export const Button = (props:ButtonProps)=>{
-    return <button>Button</button>;
+    return <button className={`${variantstyle[props.variant]} ${defaultstyle} ${sizestyle[props.size]}`}>
+        {props.starticon} {props.text} {props.endicon}
+    </button>;
 }
---
+++ src/icon/interfaceicon.ts
@@ -0,0 +1,10 @@
+export interface IconProps {
+    size: "sm" | "md" | "lg" | "xl"
+}
+
+export const iconsizevariants = {
+    "sm":"size-4",
+    "md":"size-6",
+    "lg":"size-8",
+    "xl":"size-10"
+}
