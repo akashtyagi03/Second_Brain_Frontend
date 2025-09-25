23468b437fbf42b7084f77f10110756c80a85d9c
Aakash tyagi <akashtyagi6065@gmail.com>
2025-09-26 00:03:04 +0530

    fix card component size

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -7,7 +7,7 @@ interface CardProps {
     types?: "twitter" | "youtube";
 }
 
-const defaultstyle = "p-4 rounded shadow-lg bg-white max-w-md max-h";
+const defaultstyle = "p-4 rounded shadow-lg bg-white max-w-md max-h-[350px]";
 
 export const Card = (props: CardProps) => {
     return <div className={`${defaultstyle}`}>
@@ -26,17 +26,32 @@ export const Card = (props: CardProps) => {
                 {props.title}
             </div>
             <div className="flex gap-2 items-center">
-                <ShareIcon size={"sm"} />
-                <DeleteIcon size={"sm"} />
+                <button className="cursor-pointer"><ShareIcon size="sm" /></button>
+                <button className="cursor-pointer"><DeleteIcon size="sm" /></button>
             </div>
         </div>
         {/* // here we are going to embed content of the*/}
-        <div className="pt-4">
-            {props.types === "youtube" && <iframe className="w-full" src={props.link?.replace("watch", "embed").replace("?v=", "/")} title={props.title} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>}
-
-            {props.types === "twitter" && <blockquote className="twitter-tweet">
-                <a href={props.link?.replace("x.com", "twitter.com")}></a>
-            </blockquote>}
+        <div className="pt-4 rounded-2xl">
+            {props.types === "youtube" && props.link && (
+                <iframe
+                    className="w-full aspect-video rounded"
+                    src={props.link
+                        .replace("watch?v=", "embed/")
+                        .replace("youtu.be/", "youtube.com/embed/")}
+                    title={props.title}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                ></iframe>
+            )}
+            {props.types === "twitter" && props.link && (
+                <div className="overflow-y-auto max-h-[280px]">
+                    <blockquote className="twitter-tweet">
+                        <a href={props.link.replace("x.com", "twitter.com")}></a>
+                    </blockquote>
+                </div>
+            )}
         </div>
     </div>;
 }
